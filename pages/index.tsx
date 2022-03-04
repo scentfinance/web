403f2ad4ab611d13c786bffad54e474a4275eebb
launchdex <coldenrun@gmail.com>
2022-03-04 14:55:11 -0500

    Update content and styling
    Remove particle

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,28 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Particles from "react-tsparticles";
-import { PARTICLE_OPTIONS_MULTIPLE_IMAGES } from "../constants";
+// import Particles from "react-tsparticles";
+// import { PARTICLE_OPTIONS_MULTIPLE_IMAGES } from "../constants";
 
 const Home: NextPage = () => {
-  const particlesInit = (main: any) => {
-    console.log(main);
+  // const particlesInit = (main: any) => {
+  //   console.log(main);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
+  //   // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // };
 
-  const particlesLoaded = (container: any) => {
-    console.log(container);
-  };
+  // const particlesLoaded = (container: any) => {
+  //   console.log(container);
+  // };
 
   return (
     <div className={styles.container}>
-      <Particles
+      {/* <Particles
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
         options={PARTICLE_OPTIONS_MULTIPLE_IMAGES as any}
-      />
+      /> */}
       <Head>
         <title>Scent Finance | Decentralized Art | Decentralized Finance</title>
         <meta
@@ -44,13 +44,35 @@ const Home: NextPage = () => {
 
         <div className={styles.grid}>
           <a
-            href="https://app.scent.finance"
+            href="https://founding.scent.finance"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2>Play &rarr;</h2>
-            <p>Let&apos;s get started to earn.</p>
+            <h2>Founding Member &rarr;</h2>
+            <p>Join founding member NFT drop.</p>
+          </a>
+
+          <a
+            // href="https://app.scent.finance"
+            href=""
+            className={styles.card}
+            // target="_blank"
+            // rel="noopener noreferrer"
+          >
+            <h2>Staking (coming soon) &rarr;</h2>
+            <p>Stake tokens to get benefits.</p>
+          </a>
+
+          <a
+            // href="https://app.scent.finance"
+            href=""
+            className={styles.card}
+            // target="_blank"
+            // rel="noopener noreferrer"
+          >
+            <h2>Play (coming soon) &rarr;</h2>
+            <p>Let&apos;s get started to enjoy and earn.</p>
           </a>
 
           <a
@@ -63,7 +85,7 @@ const Home: NextPage = () => {
             <p>Find in-depth information about us.</p>
           </a>
 
-          <a
+          {/* <a
             href="https://twitter.com/scentfinance"
             className={styles.card}
             target="_blank"
@@ -71,9 +93,9 @@ const Home: NextPage = () => {
           >
             <h2>Twitter &rarr;</h2>
             <p>Announcements of all events.</p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://t.me/+uIhdL6puD0M5NTM5"
             className={styles.card}
             target="_blank"
@@ -91,9 +113,9 @@ const Home: NextPage = () => {
           >
             <h2>Telegram &rarr;</h2>
             <p>Join and discover.</p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://discord.gg/SEBzR9KC4y"
             className={styles.card}
             target="_blank"
@@ -101,12 +123,38 @@ const Home: NextPage = () => {
           >
             <h2>Discord &rarr;</h2>
             <p>Join and get latest updates.</p>
-          </a>
+          </a> */}
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
+          href="https://discord.gg/7UtUCsXBd3"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.socialIcon}
+        >
+          <Image
+            src="/images/discord.svg"
+            alt="discord"
+            width={24}
+            height={24}
+          />
+        </a>
+        <a
+          href="https://twitter.com/scentfinance"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.socialIcon}
+        >
+          <Image
+            src="/images/twitter.svg"
+            alt="twitter"
+            width={24}
+            height={24}
+          />
+        </a>
+        {/* <a
           href="https://scent.finance"
           target="_blank"
           rel="noopener noreferrer"
@@ -121,7 +169,7 @@ const Home: NextPage = () => {
             />
           </span>
           Scent Finance
-        </a>
+        </a> */}
       </footer>
     </div>
   );
